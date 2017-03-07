24e087532fd5160cfc80fc5e41a34860c175af3e
Pipa007 <afalmeida.pt@gmail.com>
2017-03-07 15:47:26 +0000

    write inicial file line from .sh file

--- scripts/run_detector.sh
+++ scripts/run_detector.sh
@@ -48,6 +48,12 @@ DATASET="/media/Data/filipa/Teste_Blur"
 #BBOX=$PWD"/bbox/"
 GROUND_TRUTH_LABELS=$FILES_FOLDER_ABSOLUTE_PATH"ground_truth_labels_ilsvrc12.txt"
 
+ 
+
+echo "sigma;thres;class1;score1;x1;y1;w1;h1;class2;score2;x2;y2;w2;h2;class3;score3;x3;y3;w3;h3;class4;score4;x4;y4;w4;h4;class5;score5;x5;y5;w5;h5" > raw_bbox_parse.txt
+
+echo "sigma;thres;class1;score1;class2;score2;class3;score3;class4;score4;class5;score5;class6;score6;class7;score7;class8;score8;class9;score9;class10;score10;class11;score11;class12;score12;class13;score13;class14;score14;class15;score15;class16;score16;class17;score17;class18;score18;class19;score19;class20;score20;class21;score21;class22;score22;class23;score23;class24;score24;class25;score25">feedback_detection_parse.txt
+
 
 for SIGMA in {20,40,60,80,100}
 do
@@ -58,4 +64,6 @@ do
         build/yolt $FILES_FOLDER_ABSOLUTE_PATH $MODEL_FILE $WEIGHTS_FILE $MEAN_FILE $LABELS_FILE $SET_MODE $GPU_ID $DATASET $TOP $THRESHOLD $SIZE_MAP $LEVELS $SIGMA $GROUND_TRUTH_LABELS
     done
 done
+
+
 

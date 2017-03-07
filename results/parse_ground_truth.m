1da6e9eb7032a86c65f15c6d3731a6a3acec858f
rui <ruihortafigueiredo@gmail.com>
2017-03-07 20:11:19 +0000

    parsers finished

--- results/parse_ground_truth.m
+++ results/parse_ground_truth.m
@@ -4,7 +4,20 @@ s = dir(fullfile(gt_folder, '*.xml'));
 file_list = {s.name}';
 
 %gt = repmat(struct(), length(file_list), 1 );
-gt=[];
+gt=zeros(images_number,4);
 for i=1:images_number
-  gt = [gt; xml2struct(strcat(gt_folder,file_list{i}))];  
+    gt_ = xml2struct(strcat(gt_folder,file_list{i}));
+    if length(gt_.annotation.object)>1
+        for j=1:length(gt_.annotation.object)
+            gt(i,1)=str2num(gt_.annotation.object{j}.bndbox.xmin.Text);
+            gt(i,2)=str2num(gt_.annotation.object{j}.bndbox.ymin.Text);
+            gt(i,3)=str2num(gt_.annotation.object{j}.bndbox.xmax.Text)-gt(i,1);
+            gt(i,4)=str2num(gt_.annotation.object{j}.bndbox.ymax.Text)-gt(i,2);
+        end
+    else
+            gt(i,1)=str2num(gt_.annotation.object.bndbox.xmin.Text);
+            gt(i,2)=str2num(gt_.annotation.object.bndbox.ymin.Text);
+            gt(i,3)=str2num(gt_.annotation.object.bndbox.xmax.Text)-gt(i,1);
+            gt(i,4)=str2num(gt_.annotation.object.bndbox.ymax.Text)-gt(i,2);
+    end
 end
--- results/results.m
+++ results/results.m
@@ -1,9 +1,41 @@
-images_number=100;
 gt_folder='../dataset/gt/';
 detections_file='../dataset/detections/raw_bbox_parse.txt';
 
+% parameters
+sigmas_number=10;
+thresholds_number=10;
+images_number=100;
+overlap_correct=0.1;
 
+% get ground truth
 gt=parse_ground_truth(gt_folder,images_number);
-[sigmas,class1,class2,class3,class4,class5,score1,score2,score3,score4,score5,detections1,detections2,detections3,detections4,detections5]=parse_detections(detections_file);
 
-%bboxOverlapRatio(bboxA,bboxB)
+% get detections
+[sigmas,classes,scores,detections]=parse_detections(detections_file);
+
+% check overlaps
+overlaps=zeros(sigmas_number,thresholds_number, images_number,5);
+for s=1:sigmas_number
+    for t=1:thresholds_number
+        for i=1:images_number
+            %check overlaps
+            overlaps(s,t,i,1)=bboxOverlapRatio(gt(i,:),reshape(detections(i,1,:),1,4));
+            overlaps(s,t,i,2)=bboxOverlapRatio(gt(i,:),reshape(detections(i,2,:),1,4));
+            overlaps(s,t,i,3)=bboxOverlapRatio(gt(i,:),reshape(detections(i,3,:),1,4));
+            overlaps(s,t,i,4)=bboxOverlapRatio(gt(i,:),reshape(detections(i,4,:),1,4));
+            overlaps(s,t,i,5)=bboxOverlapRatio(gt(i,:),reshape(detections(i,5,:),1,4));
+        end
+    end
+end
+
+% consider only the maximum overlap over all 5 detections
+overlaps=max(overlaps,[],4);
+
+% evaluate if the detections were good
+overlaps(overlaps>=overlap_correct)=1;
+overlaps(overlaps<overlap_correct)=0;
+
+% compute detection rate
+detection_rate=sum(overlaps,3)/size(overlaps,3);
+
+% plots...

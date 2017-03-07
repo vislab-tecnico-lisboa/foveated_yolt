8eabdd5b67e12261c02df5a96a39c3aba15c6d23
rui <ruihortafigueiredo@gmail.com>
2017-03-07 00:10:35 +0000

    parsers implemented

--- results/results.m
+++ results/results.m
@@ -1,9 +1,9 @@
 images_number=100;
 gt_folder='../dataset/gt/';
-detections_file='../dataset/raw_bbox.txt';
+detections_file='../dataset/detections/raw_bbox_parse.txt';
 
 
 gt=parse_ground_truth(gt_folder,images_number);
-detections=parse_detections(detections_file);
+[sigmas,class1,class2,class3,class4,class5,score1,score2,score3,score4,score5,detections1,detections2,detections3,detections4,detections5]=parse_detections(detections_file);
 
 %bboxOverlapRatio(bboxA,bboxB)

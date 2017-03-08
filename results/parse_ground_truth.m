7a9dbadd1b565bfa2bed088a4445d5aef9a6e4dd
rui <ruihortafigueiredo@gmail.com>
2017-03-08 01:06:46 +0000

    plots done

--- results/parse_ground_truth.m
+++ results/parse_ground_truth.m
@@ -3,21 +3,28 @@ function gt = parse_ground_truth(gt_folder,images_number)
 s = dir(fullfile(gt_folder, '*.xml'));
 file_list = {s.name}';
 
-%gt = repmat(struct(), length(file_list), 1 );
-gt=zeros(images_number,4);
+gt=[];
 for i=1:images_number
     gt_ = xml2struct(strcat(gt_folder,file_list{i}));
     if length(gt_.annotation.object)>1
+        gt(i).bboxes=zeros(length(gt_.annotation.object),4);
         for j=1:length(gt_.annotation.object)
-            gt(i,1)=str2num(gt_.annotation.object{j}.bndbox.xmin.Text);
-            gt(i,2)=str2num(gt_.annotation.object{j}.bndbox.ymin.Text);
-            gt(i,3)=str2num(gt_.annotation.object{j}.bndbox.xmax.Text)-gt(i,1);
-            gt(i,4)=str2num(gt_.annotation.object{j}.bndbox.ymax.Text)-gt(i,2);
+            bbox=zeros(1,4);
+            bbox(1)=str2num(gt_.annotation.object{j}.bndbox.xmin.Text);
+            bbox(2)=str2num(gt_.annotation.object{j}.bndbox.ymin.Text);
+            bbox(3)=str2num(gt_.annotation.object{j}.bndbox.xmax.Text)-bbox(1);
+            bbox(4)=str2num(gt_.annotation.object{j}.bndbox.ymax.Text)-bbox(2);
+            gt(i).bboxes(j,:)=bbox;
+            
         end
     else
-            gt(i,1)=str2num(gt_.annotation.object.bndbox.xmin.Text);
-            gt(i,2)=str2num(gt_.annotation.object.bndbox.ymin.Text);
-            gt(i,3)=str2num(gt_.annotation.object.bndbox.xmax.Text)-gt(i,1);
-            gt(i,4)=str2num(gt_.annotation.object.bndbox.ymax.Text)-gt(i,2);
+        gt(i).bboxes=zeros(1,4);
+        
+        bbox=zeros(1,4);
+        bbox(1)=str2num(gt_.annotation.object.bndbox.xmin.Text);
+        bbox(2)=str2num(gt_.annotation.object.bndbox.ymin.Text);
+        bbox(3)=str2num(gt_.annotation.object.bndbox.xmax.Text)-bbox(1);     
+        bbox(4)=str2num(gt_.annotation.object.bndbox.ymax.Text)-bbox(2);
+        gt(i).bboxes=bbox;
     end
 end
--- results/results.m
+++ results/results.m
@@ -6,7 +6,7 @@ sigmas_number=10;
 thresholds_number=10;
 images_number=100;
 overlap_correct=0.1;
-
+top_k=5;
 % get ground truth
 gt=parse_ground_truth(gt_folder,images_number);
 
@@ -14,28 +14,70 @@ gt=parse_ground_truth(gt_folder,images_number);
 [sigmas,classes,scores,detections]=parse_detections(detections_file);
 
 % check overlaps
-overlaps=zeros(sigmas_number,thresholds_number, images_number,5);
+overlaps=zeros(sigmas_number,thresholds_number, images_number,top_k);
+overlaps=[];
 for s=1:sigmas_number
     for t=1:thresholds_number
         for i=1:images_number
-            %check overlaps
-            overlaps(s,t,i,1)=bboxOverlapRatio(gt(i,:),reshape(detections(i,1,:),1,4));
-            overlaps(s,t,i,2)=bboxOverlapRatio(gt(i,:),reshape(detections(i,2,:),1,4));
-            overlaps(s,t,i,3)=bboxOverlapRatio(gt(i,:),reshape(detections(i,3,:),1,4));
-            overlaps(s,t,i,4)=bboxOverlapRatio(gt(i,:),reshape(detections(i,4,:),1,4));
-            overlaps(s,t,i,5)=bboxOverlapRatio(gt(i,:),reshape(detections(i,5,:),1,4));
+            %check overlaps for each ground truth bounding box
+            overlaps(s,t,i,1).overlap=zeros(size(gt(i).bboxes,1),1);
+            for g=1:size(gt(i).bboxes,1)
+                gt_bbox=gt(i).bboxes(g,:);
+                
+                % for each detection (of the 5)
+                for j=1:top_k
+                    overlaps(s,t,i,j).overlap(g)=bboxOverlapRatio(gt_bbox,reshape(detections(i,j,:),1,4));
+                end
+            end
+        end
+    end
+end
+
+% consider only the maximum overlap over ground truths
+max_overlap=zeros(sigmas_number,thresholds_number, images_number,top_k);
+for s=1:sigmas_number
+    for t=1:thresholds_number
+        for i=1:images_number
+            for j=1:top_k
+                max_overlap(s,t,i,j)=max(overlaps(s,t,i,j).overlap);
+            end
         end
     end
 end
 
 % consider only the maximum overlap over all 5 detections
-overlaps=max(overlaps,[],4);
+max_overlap=max(max_overlap,[],4);
 
 % evaluate if the detections were good
-overlaps(overlaps>=overlap_correct)=1;
-overlaps(overlaps<overlap_correct)=0;
+max_overlap(max_overlap>=overlap_correct)=1;
+max_overlap(max_overlap<overlap_correct)=0;
 
 % compute detection rate
-detection_rate=sum(overlaps,3)/size(overlaps,3);
+detection_rate=sum(max_overlap,3)/size(max_overlap,3);
 
-% plots...
+%% plots
+
+% fix one threshold and plot all sigmas
+thresh_index=1;
+
+figure(1)
+fontsize=15;
+set(gcf, 'Color', [1,1,1]);
+plot(detection_rate(:,thresh_index))
+xlabel('$\sigma$','Interpreter','LaTex','FontSize',fontsize);
+ylabel('detection rate','Interpreter','LaTex','FontSize',fontsize);
+
+
+% fix one sigma and plot all saliency thresholds
+sigma_index=1;
+
+figure(2)
+fontsize=15;
+set(gcf, 'Color', [1,1,1]);
+plot(detection_rate(sigma_index,:))
+xlabel('$th$','Interpreter','LaTex','FontSize',fontsize);
+ylabel('detection rate','Interpreter','LaTex','FontSize',fontsize);
+
+%export_fig error_results -pdf
+
+

10d8d7bc687fb43a65607378ba066f5e20f859c2
CristinaMelicio <cristina.rodrigues.melicio@gmail.com>
2018-04-02 10:13:11 +0100

    cmake

--- cmake/Modules/FindCaffe.cmake
+++ cmake/Modules/FindCaffe.cmake
@@ -2,18 +2,16 @@
 unset(Caffe_FOUND)
 
 ###Set the variable Caffe_DIR as the root of your caffe directory
-set(Caffe_DIR ~/caffe/build/install)
+set(Caffe_DIR ~/caffe/)
 message(${Caffe_DIR})
 
 find_path(Caffe_INCLUDE_DIRS NAMES caffe/caffe.hpp caffe/common.hpp caffe/net.hpp caffe/proto/caffe.pb.h caffe/util/io.hpp caffe/vision_layers.hpp
   HINTS
-  ${Caffe_DIR}/include)
-
-
+  ${Caffe_DIR}/distribute/include)
 
 find_library(Caffe_LIBRARIES NAMES caffe
   HINTS
-  ${Caffe_DIR}/lib)
+  ${Caffe_DIR}/build/lib)
 
 message("lib_dirs:${Caffe_LIBRARIES}")
 

63a40adb27b9ffa6886083898eaa0b403964aab8
CristinaMelicio <cristina.rodrigues.melicio@gmail.com>
2018-05-18 15:48:14 +0100

    matlab scripts updated

--- test/matlab_cris/classification_error_rates.m
+++ test/matlab_cris/classification_error_rates.m
@@ -1,4 +1,4 @@
-function [tp1_class_error_av,tp1_class_error_std,tp5_class_error_av,tp5_class_error_std] = classification_error_rates(sigmas,threshs,fix_pts, images_number,classes,gt_classes, top_k)
+function [tp1_class_error_av,tp1_class_error_std,tp5_class_error_av,tp5_class_error_std,class_error_1pos,class_error_5pos] = classification_error_rates(sigmas,threshs,fix_pts, images_number,classes,gt_classes, top_k)
 
 top1_classification=zeros(length(sigmas),length(threshs),size(fix_pts,1),images_number);
 top5_classification=zeros(length(sigmas),length(threshs),size(fix_pts,1),images_number);
@@ -17,15 +17,18 @@ for s=1:length(sigmas)
         end
     end
 end
-top1_classification_error_rate = (size(top1_classification,4)-sum(top1_classification,4))/(size(top1_classification,4));
+
+top1_classification_error_rate=((size(top1_classification,4))-...
+    sum(top1_classification,4))/(size(top1_classification,4));
 tp1_class_error_av = mean(top1_classification_error_rate,3);
 tp1_class_error_std = std(top1_classification_error_rate,1,3);
 
-top5_classification_error_rate = (size(top5_classification,4)-sum(top5_classification,4))/(size(top5_classification,4));
+top5_classification_error_rate=((size(top5_classification,4))-...
+    sum(top5_classification,4))/(size(top5_classification,4));
 tp5_class_error_av = mean(top5_classification_error_rate,3);
 tp5_class_error_std = std(top5_classification_error_rate,1,3);
 
-% compute classification error rate
-%top1_classification_error_rate=(size(top1_classification,3)*size(top1_classification,4)-sum(sum(top1_classification,4),3))/(size(top1_classification,4)*size(top1_classification,3));
-%top5_classification_error_rate=(size(top5_classification,3)*size(top5_classification,4)-sum(sum(top5_classification,4),3))/(size(top5_classification,4)*size(top5_classification,3));
-
+class_error_5pos=((size(top5_classification,4))*(size(top5_classification,2))*(size(top5_classification,1))-...
+    sum(sum(sum(top5_classification,4),2),1))/((size(top5_classification,4))*(size(top5_classification,2))*(size(top5_classification,1)));
+class_error_1pos=((size(top1_classification,4))*(size(top1_classification,2))*(size(top1_classification,1))-...
+    sum(sum(sum(top1_classification,4),2),1))/((size(top1_classification,4))*(size(top1_classification,2))*(size(top1_classification,1)));
